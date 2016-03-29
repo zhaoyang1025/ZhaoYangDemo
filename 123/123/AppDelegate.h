579a0231c8a4042854d30b71edfdad4f3bb1391c
mac <mac@macdemacbook-air.local>
2016-03-29 14:49:20 +0800

    添加一个新的工程

---
+++ 123/123/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  123
+//
+//  Created by mac on 16/3/29.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 123/123/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  123
+//
+//  Created by mac on 16/3/29.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
